f47dd99029493060909a7a283923896430745dc5
subpuri <subhpuri0@gmail.com>
2025-05-16 14:56:48 +0530

    adding input-output.cpp to git TUF

---
+++ input-output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   cout <<"Hello world!" <<"\n";
+   cout <<"!!" <<endl;
+   int x,y;
+   cout << "enter value of x and y" <<endl;
+   cin >> x >>y;
+   cout <<"value of x is "<<x<<endl<<"value of y is " <<y<<endl;
+   return 2;
+}
