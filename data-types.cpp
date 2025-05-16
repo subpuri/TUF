03ba58589da9c771e691302ffe9dd9bfea2d46d5
subpuri <subhpuri0@gmail.com>
2025-05-16 15:56:41 +0530

    adding data-types.cpp to TUF

---
+++ data-types.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int age = 25;
+	float hight = 3.5;
+	double pi = 3.1415926535f;
+	char grade = 'A';
+	bool isPassed = true;
+	string name = "Subhash";
+	cout << "Name: "<<name<<endl;
+	cout <<"Age: "<<age<<endl;
+	cout <<"hight: "<<hight<<endl;
+	cout <<"pi: "<<pi<<endl;
+	cout <<"Grade: "<<grade<<endl;
+	cout <<"Passed: "<<isPassed<<endl;
+	cout <<"size of int: "<<sizeof(int)<<"bytes"<<endl;
+	cout <<"size of float: "<<sizeof(float)<<"bytes"<<endl;
+	cout <<"size of double: "<<sizeof(double)<<"bytes"<<endl;
+	cout <<"size of char: "<<sizeof(char)<<"bytes"<<endl;
+	cout <<"size of bool: "<<sizeof(bool)<<"bytes"<<endl;
+	cout <<"size of string: "<<sizeof(string)<<"bytes"<<endl;
+
+	return 0;
+}
